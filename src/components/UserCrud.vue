db14d61c353542b12f456734c834fc85c196c2b1
mjvamorim <mjvamorim@gmail.com>
2022-04-05 19:33:11 -0300

    Consumindo API com Axios

--- src/components/UserCrud.vue
+++ src/components/UserCrud.vue
@@ -1,10 +1,45 @@
 <template>
-  <h1>UserCrud</h1>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+           <h1> UserCrud </h1>
+           <v-data-table
+              :headers="headers"
+              :items="users"
+              :search="search"
+              sort-by="id"
+              class="elavation-1">
+           </v-data-table>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
+import axios from "axios"
+
 export default {
   name: "UserCrud",
-  data: () => ({}),
-  methods: {},
+  data: () => ({
+    search:"",
+    headers: [
+      { text: "Id", value: "id"},
+      { text: "Nome", value: "name"},
+      { text: "País", value: "location"},
+      { text: "Actions", value: "actions", sortable: false}
+    ],
+    users: []
+  }),
+  methods: {
+    inicializa() {
+      axios.get('http://localhost:3000/users')
+        .then((response) => {
+          this.users = response.data
+        })
+        .catch((error)=> console.log(error))
+    }
+  },
+  created() {
+    this.inicializa()
+  }
 };
 </script>
